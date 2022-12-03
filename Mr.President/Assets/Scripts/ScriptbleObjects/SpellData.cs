27041da1b27ac2c759621603f568cd79b1a3936b
skyhex-spr <skyhex.spr@gmail.com>
2022-12-03 20:19:38 +0330

    Spells Data Card sample

---
+++ Mr.President/Assets/Scripts/ScriptbleObjects/SpellData.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SpellsData", menuName = "ScriptableObjects/SpellsData", order = 2)]
+public class SpellData : ScriptableObject
+{
+    public List<Spell> Spells = new List<Spell>();
+
+    private void OnValidate()
+    {
+        setIds(Spells);
+    }
+
+    private void setIds(List<Spell> list)
+    {
+        if (list.Count > 1)
+        {
+            list[0].Id = 0;
+            for (int i = 1; i < list.Count; i++)
+            {
+                list[i].Id = list[i - 1].Id + 1;
+            }
+        }
+        else
+        if (list.Count == 1)
+        {
+            list[0].Id = 0;
+        }
+    }
+
+}
+
+[System.Serializable]
+public class Spell 
+{
+    public int Id;
+    public string Name;
+    [Space(10)]
+    public bool Percent_Effect = false;
+    [DrawIf("Percent_Effect", true)]
+    public float PercentEffect;
+    public bool Count_Effect = false;
+    [DrawIf("Count_Effect", true)]
+    public float CountEffect;
+    [Space(10)]
+    public float Percent_to_Spell;
+    public int Times_to_Spell;
+    [Space(10)]
+    [Header("Add SpecialMinisters")]
+    public List<MinistersID> SpecialMinisters = new List<MinistersID>();
+    [Space(10)]
+    public int Cards_Count;
+
+}
+
+[System.Serializable]
+public class MinistersID
+{
+    public int id;
+}
+
